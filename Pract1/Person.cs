929c8b4c809166afd61be0778be0d35ed64059d5
Elisey <shemetov.elisey@gmail.com>
2020-09-12 12:15:28 +0700

    moving the Person class to a separate file

---
+++ Pract1/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Pract1
+{
+    public class Person
+    {
+        private string sName;
+        private string fName;
+        private string patr;
+        private float hight;
+        private float weight;
+        private DateTime bDate;
+
+        public Person(string sName, string fName, string patr, float hight,
+            float weight, DateTime bDate)
+        {
+            this.sName = sName;
+            this.fName = fName;
+            this.patr = patr;
+            this.hight = hight;
+            this.weight = weight;
+            this.bDate = bDate;
+        }
+
+        public Byte getAge()
+        {
+            return 0;
+        }
+
+    }
+}
--- Pract1/Program.cs
+++ Pract1/Program.cs
@@ -2,35 +2,6 @@
 
 namespace Pract1
 {
-    class Person
-    {
-        private string sName;
-        private string fName;
-        private string patr;
-        private float hight;
-        private float weight;
-        private DateTime bDate;
-
-        public Person(string sName, string fName, string patr, float hight,
-            float weight, DateTime bDate)
-        {
-            this.sName = sName;
-            this.fName = fName;
-            this.patr = patr;
-            this.hight = hight;
-            this.weight = weight;
-            this.bDate = bDate;
-        }
-
-        public Byte getAge()
-        {
-            return 0;
-        }
-
-
-    }
-
-
     class Program
     {
         static void Main(string[] args)
