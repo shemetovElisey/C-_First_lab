a73053236e4f3d2e76701ee7728abf02a6f05edb
Elisey <shemetov.elisey@gmail.com>
2020-09-13 14:46:03 +0700

    add input/output functional

--- Pract1/Person.cs
+++ Pract1/Person.cs
@@ -3,28 +3,44 @@ namespace Pract1
 {
     public class Person
     {
-        private string sName;
-        private string fName;
-        private string patr;
-        private float hight;
-        private float weight;
-        private DateTime bDate;
+        public string SName { get; }
+        public string FName { get; }
+        public string Patr { get; }
+        public double Hight { get; }
+        public double Weight { get; }
+        public DateTime Birth { get; }
 
-        public Person(string sName, string fName, string patr, float hight,
-            float weight, DateTime bDate)
+        public Person(string sName, string fName, string patr, double hight,
+            double weight, DateTime birth)
         {
-            this.sName = sName;
-            this.fName = fName;
-            this.patr = patr;
-            this.hight = hight;
-            this.weight = weight;
-            this.bDate = bDate;
+            this.SName = sName;
+            this.FName = fName;
+            this.Patr = patr;
+            this.Hight = hight;
+            this.Weight = weight;
+            this.Birth = birth;
         }
 
-        public Byte getAge()
+        public byte GetAge()
         {
-            return 0;
+            try
+            {
+                if (DateTime.Now.Month > Birth.Month)
+                    return Convert.ToByte(DateTime.Now.Year - Birth.Year);
+                else if (DateTime.Now.Year == Birth.Year)
+                    return Convert.ToByte(DateTime.Now.Date < Birth.Date ?
+                        DateTime.Now.Year - Birth.Year - 1 :
+                        DateTime.Now.Year - Birth.Year);
+                else return Convert.ToByte(DateTime.Now.Year - Birth.Year - 1);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Возраст больше 255");
+                throw;
+            }
         }
+
+
 
     }
 }
--- Pract1/Program.cs
+++ Pract1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
 
 namespace Pract1
 {
@@ -6,7 +10,39 @@ namespace Pract1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Person> people = new List<Person>();
+
+            using StreamReader inF = new StreamReader("in.txt", Encoding.UTF8);
+            while (inF.Peek() != -1)
+            {
+                string[] things = inF.ReadLine().Split(' ');
+                people.Add(new Person(things[0], things[1], things[2],
+                    (float)Convert.ToDouble(things[3]),
+                (float)Convert.ToDouble(things[4]),
+                DateTime.ParseExact(things[5], "dd/MM/yyyy",
+                System.Globalization.CultureInfo.InvariantCulture)));
+            }
+
+            Console.WriteLine("Выберите направление сортировки\nПо возрастанию: 1\n" +
+                "По убыванию: 2");
+            string flag = Console.ReadLine();
+
+            if (Convert.ToInt32(flag) == 1)
+                people.Sort((x, y) => x.SName.CompareTo(y.SName));
+            else
+                people.Sort((x, y) => y.SName.CompareTo(x.SName));
+
+            FileStream output = File.OpenWrite("out.txt");
+            using StreamWriter f = new StreamWriter(output, Encoding.UTF8);
+            {
+                CultureInfo ruRU = new CultureInfo("ru-RU");
+                foreach (Person var in people)
+                    f.WriteLine("{0,-18}{1,-18}{2,-18}{3,-9}{4,-13}{5, -13}{6}",
+                        var.SName, var.FName, var.Patr, var.GetAge(),
+                        var.Weight.ToString("e3"),
+                        var.Hight.ToString("e3"),
+                        var.Birth.ToString("dd MMMM yyyy", ruRU).ToLower());
+            }
         }
     }
 }
