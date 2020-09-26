549578a67f7963b92e86dfea80918a29697b90b7
Elisey <shemetov.elisey@gmail.com>
2020-09-26 11:30:40 +0700

    fix 2

--- Pract1/Person.cs
+++ Pract1/Person.cs
@@ -1,43 +1,43 @@
 ﻿using System;
 namespace Pract1
 {
-    public class Person
-    {
-        public string SName { get; }
-        public string FName { get; }
-        public string Patr { get; }
-        public double Hight { get; }
-        public double Weight { get; }
-        public DateTime Birth { get; }
+   public class Person
+   {
+      public string SName { get; }
+      public string FName { get; }
+      public string Patr { get; }
+      public double Hight { get; }
+      public double Weight { get; }
+      public DateTime Birth { get; }
 
-        public Person(string sName, string fName, string patr, double hight,
-            double weight, DateTime birth)
-        {
-            this.SName = sName;
-            this.FName = fName;
-            this.Patr = patr;
-            this.Hight = hight;
-            this.Weight = weight;
-            this.Birth = birth;
-        }
+      public Person(string sName, string fName, string patr, double hight,
+          double weight, DateTime birth)
+      {
+         this.SName = sName;
+         this.FName = fName;
+         this.Patr = patr;
+         this.Hight = hight;
+         this.Weight = weight;
+         this.Birth = birth;
+      }
 
-        public byte GetAge()
-        {
-            try
-            {
-                if (DateTime.Now.Month > Birth.Month)
-                    return Convert.ToByte(DateTime.Now.Year - Birth.Year);
-                else if (DateTime.Now.Year == Birth.Year)
-                    return Convert.ToByte(DateTime.Now.Date < Birth.Date ?
-                        DateTime.Now.Year - Birth.Year - 1 :
-                        DateTime.Now.Year - Birth.Year);
-                else return Convert.ToByte(DateTime.Now.Year - Birth.Year - 1);
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Возраст больше 255 или меньше 0");
-                throw;
-            }
-        }
-    }
+      public byte GetAge()
+      {
+         try
+         {
+            if (DateTime.Now.Month > Birth.Month)
+               return Convert.ToByte(DateTime.Now.Year - Birth.Year);
+            else if (DateTime.Now.Year == Birth.Year)
+               return Convert.ToByte(DateTime.Now.Date < Birth.Date ?
+                   DateTime.Now.Year - Birth.Year - 1 :
+                   DateTime.Now.Year - Birth.Year);
+            else return Convert.ToByte(DateTime.Now.Year - Birth.Year - 1);
+         }
+         catch (OverflowException)
+         {
+            Console.WriteLine("Возраст больше 255 или меньше 0");
+            throw;
+         }
+      }
+   }
 }
--- Pract1/Program.cs
+++ Pract1/Program.cs
@@ -6,54 +6,52 @@ using System.Text;
 
 namespace Pract1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<Person> people = new List<Person>();
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         List<Person> people = new List<Person>();
 
-            using StreamReader inF = new StreamReader("in.txt", Encoding.UTF8);
-            try
+         using StreamReader inF = new StreamReader("in.txt", Encoding.UTF8);
+         try
+         {
+            while (inF.Peek() != -1)
             {
-                while (inF.Peek() != -1)
-                {
-                    string[] things = inF.ReadLine().Split(' ');
-                    people.Add(new Person(things[0], things[1], things[2],
-                        (float)Convert.ToDouble(things[3]),
-                    (float)Convert.ToDouble(things[4]),
-                    DateTime.ParseExact(things[5], "dd/MM/yyyy",
-                    System.Globalization.CultureInfo.InvariantCulture)));
-                }
+               string[] things = inF.ReadLine().Split(' ');
+               people.Add(new Person(things[0], things[1], things[2],
+                   (float)Convert.ToDouble(things[3]),
+               (float)Convert.ToDouble(things[4]),
+               DateTime.ParseExact(things[5], "dd/MM/yyyy",
+               System.Globalization.CultureInfo.InvariantCulture)));
             }
+         }
+         catch (FormatException)
+         {
+            Console.WriteLine("Данные введенны неверно");
+            throw;
+         }
 
-            catch (FormatException)
-            {
-                Console.WriteLine("Данные введенны неверно");
-                throw;
-            }
+         Console.WriteLine("Выберите направление сортировки\nПо возрастанию: 1\n" +
+             "По убыванию: 2");
+         string flag = Console.ReadLine();
 
-            Console.WriteLine("Выберите направление сортировки\nПо возрастанию: 1\n" +
-                "По убыванию: 2");
-            string flag = Console.ReadLine();
+         if (Convert.ToInt32(flag) == 1)
+            people.Sort((x, y) => x.SName.CompareTo(y.SName));
+         else
+            people.Sort((x, y) => y.SName.CompareTo(x.SName));
 
-            if (Convert.ToInt32(flag) == 1)
-                people.Sort((x, y) => x.SName.CompareTo(y.SName));
-            else
-                people.Sort((x, y) => y.SName.CompareTo(x.SName));
-
-            FileStream output = File.OpenWrite("out.txt");
-            using StreamWriter f = new StreamWriter(output, Encoding.UTF8);
-            {
-                CultureInfo ruRU = new CultureInfo("ru-RU");
-                foreach (Person var in people)
-                    f.WriteLine($"{var.SName,-18}{var.FName,-18}" +
-                        $"{var.Patr,-18}{var.GetAge(),-9}" +
-                        $"{var.Weight.ToString("e3"),-13}" +
-                        $"{var.Hight.ToString("e3"), -13}" +
-                        $"{var.Birth.ToString("dd MMMM yyyy", ruRU).ToLower()}");
-            }
-        }
-    }
+         FileStream output = File.OpenWrite("out.txt");
+         using StreamWriter f = new StreamWriter(output, Encoding.UTF8);
+         {
+            CultureInfo ruRU = new CultureInfo("ru-RU");
+            foreach (Person var in people)
+               f.WriteLine($"{var.SName,-18}{var.FName,-18}" +
+                   $"{var.Patr,-18}{var.GetAge(),-9}" +
+                   $"{var.Weight,-13:e3}" +
+                   $"{var.Birth.ToString("dd MMMM yyyy", ruRU).ToLower()}");
+         }
+      }
+   }
 }
 
 
